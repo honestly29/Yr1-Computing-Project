02001fbba43a28ee08ffe2ee46c169deefcfa453
Roan Jones <honestly2978@gmail.com>
2024-03-03 15:46:10 +0000

    Added drawing app files

---
+++ lineToTool.js
@@ -0,0 +1,42 @@
+// While mouse is held down, draw a line between where mouse is pressed and where mouse is let go
+
+function LineToTool(){
+	this.icon = "assets/lineTo.jpg";
+	this.name = "LineTo";
+
+	// variables to store position of the mouse
+	var startMouseX = -1;
+	var startMouseY = -1;
+
+	// boolean to show drawing state
+	var drawing = false;
+
+	this.draw = function(){
+
+		if(mouseIsPressed){
+			if(startMouseX == -1){
+				startMouseX = mouseX;
+				startMouseY = mouseY;
+				drawing = true;
+				// loads the current colour value (RGBA) of each pixel on the canvas into the pixels array
+				loadPixels();
+			}
+
+			else{
+				// updates the canvas with the new RGBA values in the pixels array.
+				updatePixels();
+				line(startMouseX, startMouseY, mouseX, mouseY);
+			}
+
+		}
+
+		// when mouse is let go, stops drawing the line and sets x and y back to -1
+		else if(drawing){
+			drawing = false;
+			startMouseX = -1;
+			startMouseY = -1;
+		}
+	};
+
+
+}
